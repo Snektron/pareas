b6c73acb70216169d149cd77271eefd1cf180d3c
Robin Voetter <robin@voetter.nl>
2021-01-21 02:04:35 +0100

    Initialize futhark context & invoke basic kernel

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <futhark-generated.h>
 #include <iostream>
 #include <string_view>
+#include <memory>
 #include <cstdlib>
+#include <cstdint>
 
 struct Options {
     const char* input_path;
@@ -45,7 +47,7 @@ bool parse_options(Options* opts, int argc, const char* argv[]) {
             opts->help = true;
         } else if (arg == "-v" || arg == "--verbose") {
             opts->verbose = true;
-        } else if (arg == "--debug") {
+        } else if (arg == "-d" || arg == "--debug") {
             opts->debug = true;
         } else if (!opts->input_path) {
             opts->input_path = argv[i];
@@ -74,6 +76,26 @@ bool parse_options(Options* opts, int argc, const char* argv[]) {
     return true;
 }
 
+template <typename T, void(*deleter)(T*)>
+struct Deleter {
+    void operator()(T* t) const {
+        deleter(t);
+    }
+};
+
+template <typename T, void(*deleter)(T*)>
+using UniqueCPtr = std::unique_ptr<T, Deleter<T, deleter>>;
+
+template <typename T>
+struct Free {
+    void operator()(T* ptr) const {
+        free(static_cast<void*>(ptr));
+    }
+};
+
+template <typename T>
+using MallocPtr = std::unique_ptr<T, Free<T>>;
+
 int main(int argc, const char* argv[]) {
     Options opts;
     if (!parse_options(&opts, argc, argv)) {
@@ -83,6 +105,25 @@ int main(int argc, const char* argv[]) {
         print_usage(argv[0]);
         return EXIT_SUCCESS;
     }
+
+    auto config = UniqueCPtr<futhark_context_config, futhark_context_config_free>(futhark_context_config_new());
+    futhark_context_config_set_logging(config.get(), opts.verbose);
+    futhark_context_config_set_debugging(config.get(), opts.debug);
+
+    auto context = UniqueCPtr<futhark_context, futhark_context_free>(futhark_context_new(config.get()));
+
+    int64_t result;
+    int err = futhark_entry_main(context.get(), &result, 10);
+    if (!err)
+        err = futhark_context_sync(context.get());
+
+    if (err) {
+        auto err = MallocPtr<char>(futhark_context_get_error(context.get()));
+        std::cerr << "Kernel error: " << (err ? err.get() : "(no diagnostic)") << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::cout << "Result: " << result << std::endl;
 
     return EXIT_SUCCESS;
 }
--- src/main.fut
+++ src/main.fut
@@ -1,6 +1,8 @@
 module tree = import "tree"
 
-let main (n : i64) : u32 =
-    let t = tree.alloc_tree n
-    let result = tree.walk_tree t
-    in result
+-- let main (n : i64) : u32 =
+--     let t = tree.alloc_tree n
+--     let result = tree.walk_tree t
+--     in result
+
+let main (n: i64): i64 = n * 2
