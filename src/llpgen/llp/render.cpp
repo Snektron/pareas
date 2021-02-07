9a7056468ae6d0377fabfafa2bc03fdc45d7053f
Robin Voetter <robin@voetter.nl>
2021-02-07 16:55:46 +0100

    Restructure compiler sources in to subdirectory

---
+++ src/llpgen/llp/render.cpp
@@ -0,0 +1,13 @@
+#include "pareas/llpgen/llp/render.hpp"
+
+#include <ostream>
+
+namespace {
+
+}
+
+namespace llp {
+    void render_parser(std::ostream& out, const ParsingTable& pt) {
+
+    }
+}
