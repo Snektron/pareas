c5e3533757137f1e0996f44fe71abadad622e6d3
Robin Voetter <robin@voetter.nl>
2021-07-10 19:46:56 +0200

    Remove some unused functions

--- include/pareas/compiler/futhark_interop.hpp
+++ include/pareas/compiler/futhark_interop.hpp
@@ -149,22 +149,6 @@ namespace futhark {
             if (err != 0)
                 throw Error(this->handle.ctx);
         }
-
-        std::vector<T> download() const {
-            auto* shape = this->shape();
-            int64_t total_size = 1;
-            for (size_t i = 0; i < N; ++i) {
-                total_size *= shape[i];
-            }
-
-            auto result = std::vector<T>(total_size);
-            this->values(result.data());
-            return result;
-        }
-
-        const int64_t* shape() const {
-            return ArrayTraits<T, N>::shape_fn(this->handle.ctx, this->handle.data);
-        }
     };
 
     template <>
